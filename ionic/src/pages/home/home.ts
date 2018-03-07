270542235693f17f72b4ee1c280b07384916a06c
Jessica Liu <pncklypear@users.noreply.github.com>
2018-03-06 18:50:53 -0600

    home.ts: added swipe & search bar test
    swipe left to go to create a post view
    typing anything in the search bar will trigger an alert if the searchbar works

--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PostPage } from '../post/post';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   }
+
+
+  getItems(event){
+    let alert = this.alertCtrl.create({
+      title: 'Testing Searchbar!',
+      subTitle: 'This popup means that everything is working!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+  swipeLeftEvent(event) {
+      this.navCtrl.push(PostPage);
+  }
+
 
 }
